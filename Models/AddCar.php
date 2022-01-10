b48560140a9f3a084f36330c43c6d783afb82948
Heba Elwazzan <hebaelwazan@gmail.com>
2022-01-10 06:59:54 +0200

    add car

---
+++ Models/AddCar.php
@@ -0,0 +1,28 @@
+<?php   
+
+
+$conn = mysqli_connect('localhost', 'root', '', 'car_rental_system');
+
+if (!$conn) {
+    die("<script>alert('Connection to database failed.')</script>");
+}
+if (isset($_POST['add-car'])) {
+
+
+    $plate_number = $_POST['plate_number'];
+    $model = $_POST['model'];
+    $brand = $_POST['brand'];
+    $year = $_POST['year'];
+    $color = $_POST['color'];
+    $price_per_day = $_POST['price_per_day'];
+    $office_id = $_POST['office_id'];
+     
+
+    $SQL = "INSERT INTO Car (plate_number,model,brand,`year`,color,price_per_day,office_id, status, is_reserved) VALUES 
+    (".$plate_number.",".$model.",".$brand.",".$year.",".$color.",".$price_per_day.",".$office_id.", 'active', 'N')";
+
+
+    $result = mysqli_query($conn, $sql);
+    echo "<script>window.location.href = '../View/edit_car.php'; alert('Car added successfully');</script>";
+}
+?>
--- View/add_car.php
+++ View/add_car.php
@@ -1,3 +1,55 @@
-<?php
-include "admin_header.php"
+<?php 
+include "admin_header.php";
 ?>
+<html>
+    <h2>Add a car</h2>
+    <div class="col-sm-6 col-sm-offset-3 form-box">
+
+        <form name="add-car" action="../Models/AddCar.php" method="POST">
+            
+            <div class="form-group">
+                <label>Plate Number</label>
+                <input type="text" class="form-control" name="plate_number" required/>
+            </div>
+
+            <div class="form-group">
+                <label>Model</label>
+                <input type="text" class="form-control" name="model" required/>
+            </div>
+
+            <div class="form-group">
+                <label>brand</label>
+                <input type="text" class="form-control" name="brand" required/>
+            </div>
+
+            <div class="form-group">
+                <label>year</label>
+                <input type="text" class="form-control" name="year" required/>
+            </div>
+
+
+            <div class="form-group">
+                <label>price per day</label>
+                <input type="text" class="form-control" name="price_per_day" required/>
+            </div>
+            
+            <div class="form-group">
+                <label>Office_ID</label>
+                <input type="text" class="form-control" name="office_id" required/>
+            </div>
+            
+            <div class="form-group">
+                <label>Color</label>
+                <input type="text" class="form-control" name="color" required/>
+            </div>
+
+
+
+
+            <button type="submit" class="btn btn-success form-button" name = "add-car">
+                Add
+            </button>
+        </form>
+    </div>
+</body>
+</html>
